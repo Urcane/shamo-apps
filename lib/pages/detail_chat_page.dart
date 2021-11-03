016628b4f0e83f4bd0b0a6bdada03acd9a5bab48
Mulyadi <mulyadi@PTs-MacBook-Pro.local>
2021-11-03 16:21:53 +0800

    re-slicing: fix chat input

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -120,7 +120,7 @@ class DetailChatPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            _productPreview(),
+            // _productPreview(),
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
@@ -192,8 +192,22 @@ class DetailChatPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: background3Color,
       appBar: header(),
-      bottomNavigationBar: chatInput(),
-      body: content(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Stack(
+              children: <Widget>[
+                content(),
+                Align(
+                  child: _productPreview(),
+                  alignment: Alignment.bottomLeft,
+                ),
+              ],
+            ),
+          ),
+          chatInput(),
+        ],
+      ),
     );
   }
 }
